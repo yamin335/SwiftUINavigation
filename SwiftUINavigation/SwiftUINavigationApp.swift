eab7ddddaf6ad28d8c126b788a5cfef19e8192f0
Yamin <myamin335@gmail.com>
2023-05-10 14:28:04 +0200

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Md. Yamin on 10.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
